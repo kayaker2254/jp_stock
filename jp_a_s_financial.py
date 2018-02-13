7f92bbabf61ce760ad876966324c96dd21bd9a4b
Shin <kayaker219@yahoo.co.jp>
2018-02-13 23:44:27 +0900

    Stock price data acquisition and storage in database

--- jp_a_s_financial.py
+++ jp_a_s_financial.py
@@ -5,14 +5,37 @@ from sqlalchemy import create_engine
 import os
 from datetime import datetime,timedelta
 
-os.system('wget --http-user="trial" --http-passwd="PW@20170129" "https://hesonogoma.com/stocks/download/csv/japan-all-stock-financial-results/monthly/japan-all-stock-financial-results.csv" -P /Users/shin/Downloads/ -NP /Users/shin/Downloads/ -N')
-os.system('nkf -w --overwrite /Users/shin/Downloads/japan-all-stock-financial-results.csv')
+os.system(\
+        'wget --http-user="trial" \
+        --http-passwd="PW@20170129" \
+        "https://hesonogoma.com/stocks/download/csv/japan-all-stock-financial-results/monthly/japan-all-stock-financial-results.csv" \
+        -P /Users/shin/Downloads/ \
+        -NP /Users/shin/Downloads/ \
+        -N'\
+        )
+
+os.system(\
+        'nkf \
+        -w \
+        --overwrite \
+        /Users/shin/Downloads/japan-all-stock-financial-results.csv'\
+        )
+
 fi_dt = datetime.fromtimestamp(os.stat('/Users/shin/Downloads/japan-all-stock-financial-results.csv').st_mtime)
 fi_get_time = fi_dt.strftime('%Y-%m-%d %H:%M:%S')
 
-columns_fi = ['st_code','name','kessannki','kessannhappyoubi','uriagedaka','eigyourieki','keijyourieki','toukirieki','sousisann','jikosihonn','sihonnkinn','yuurisihusai','jikosihonnhiritu','roe','roa','hakkouzumikabusikisuu']
+columns_fi = [\
+        'st_code','name','kessannki','kessannhappyoubi','uriagedaka','eigyourieki','keijyourieki',\
+        'toukirieki','sousisann','jikosihonn','sihonnkinn','yuurisihusai','jikosihonnhiritu','roe',\
+        'roa','hakkouzumikabusikisuu'\
+        ]
 
-fi_df = pd.read_csv('/Users/shin/Downloads/japan-all-stock-financial-results.csv', skiprows = 1, na_values=('-',''), names = columns_fi )
+fi_df = pd.read_csv(\
+        '/Users/shin/Downloads/japan-all-stock-financial-results.csv', \
+        skiprows = 1, \
+        na_values=('-',''), \
+        names = columns_fi \
+        )
 
 n = len(fi_df)
 fi_df['fi_get_time']= [fi_get_time]*n
@@ -20,4 +43,10 @@ fi_df['fi_get_time'] = pd.to_datetime(fi_df['fi_get_time'])
 
 engine = create_engine('postgresql://shinya@localhost:5432/stock')
 
-fi_df.to_sql('jp_al_st_financial', engine, if_exists = 'append', index=False)
+fi_df.to_sql(\
+        'jp_al_st_financial', \
+        engine, \
+        if_exists = 'append', \
+        index=False\
+        )
+
