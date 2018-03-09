efb4f67f0841ca8427df09e331c40cd8dc54382d
Shin <kayaker219@yahoo.co.jp>
2018-03-09 23:06:00 +0900

    Stock price data acquisition and storage in database

--- jp_a_s_data_daily.py
+++ jp_a_s_data_daily.py
@@ -7,14 +7,14 @@ from datetime import datetime,timedelta
 sys.path.append('/Users/shin/stubby')
 from csvex import *
 
-os.system(('wget\
-        --http-user={0}\
-        --http-passwd={1}\
+os.system(('wget \
+        --http-user={0} \
+        --http-passwd={1} \
         "https://csvex.com/kabu.plus/csv/japan-all-stock-data/daily/japan-all-stock-data.csv" \
         -P /Users/shin/Downloads/ \
         -NP /Users/shin/Downloads/ \
         -N'\
-        ) \
+        )\
         .format(csvex_id,csvex_pass))
 
 download_time = datetime.fromtimestamp(os.stat('/Users/shin/Downloads/japan-all-stock-data.csv').st_mtime)
@@ -24,23 +24,22 @@ get_date = download_time.strftime('%Y-%m-%d %H:%M:%S')
 os.system('nkf \
         -w \
         --overwrite \
-        /Users/shin/Downloads/japan-all-stock-data.csv'\
-        )
+        /Users/shin/Downloads/japan-all-stock-data.csv')
 
 csv_file = '/Users/shin/Downloads/japan-all-stock-data.csv'
 
-jp_al_st__dd_colmns = [\
-        'st_code', 'name', 'market', 'industry', 'jika_sougaku', 'hakkouzumi_kabusikisuu', \
-        'haitou_rimawari', 'hitokabu_haitou', 'per_yosoku', 'pbr_jisseki', 'eps_yosoku', \
-        'bps_jisseki', 'saitei_kounyuugaku', 'tanngennkabu', 'takane_hiduke', 'nennsyorai_takane', \
-        'yasune_hiduke', 'nennsyorai_yasune'\
+jp_al_st__dd_colmns = [
+        'st_code', 'name', 'market', 'industry', 'jika_sougaku', 'hakkouzumi_kabusikisuu',
+        'haitou_rimawari', 'hitokabu_haitou', 'per_yosoku', 'pbr_jisseki', 'eps_yosoku',
+        'bps_jisseki', 'saitei_kounyuugaku', 'tanngennkabu', 'takane_hiduke', 'nennsyorai_takane',
+        'yasune_hiduke', 'nennsyorai_yasune'
         ]
 
 #1行目は指標名、2行目、3行目は株価指数の為、読み飛ばす。
-data_daily = pd.read_csv(\
-        csv_file, skiprows = 3, \
-        na_values=('-',''), \
-        names = jp_al_st__dd_colmns \
+data_daily = pd.read_csv(
+        csv_file, skiprows = 3,
+        na_values=('-',''),
+        names = jp_al_st__dd_colmns
         )
 
 data_daily['takane_hiduke'] = pd.to_datetime(data_daily['takane_hiduke'])
@@ -53,8 +52,8 @@ data_daily['get_date'] = pd.to_datetime(data_daily['get_date'])
 engine = create_engine('postgresql://shinya@localhost:5432/stock')
 
 data_daily.to_sql(
-        'jp_al_st_data_daily', \
-        engine, \
-        if_exists = 'append', \
-        index=False \
+        'jp_al_st_data_daily',
+        engine,
+        if_exists = 'append',
+        index=False
         )
--- jp_a_s_financial.py
+++ jp_a_s_financial.py
@@ -7,37 +7,37 @@ from datetime import datetime,timedelta
 sys.path.append('/Users/shin/stubby')
 from csvex import *
 
-os.system((\
-        'wget --http-user={0} \
-        --http-passwd={1} \
-        "https://csvex.com/kabu.plus/csv/japan-all-stock-financial-results/monthly/japan-all-stock-financial-results.csv"
-        -P /Users/shin/Downloads/ \
-        -NP /Users/shin/Downloads/ \
-        -N'\
-        ) \
+os.system((
+        'wget --http-user={0}
+         --http-passwd={1}
+         "https://csvex.com/kabu.plus/csv/japan-all-stock-financial-results/monthly/japan-all-stock-financial-results.csv"
+         -P /Users/shin/Downloads/
+         -NP /Users/shin/Downloads/
+         -N'
+        )
         .format(csvex_id,csvex_pass))
 
-os.system(\
-        'nkf \
-        -w \
-        --overwrite \
-        /Users/shin/Downloads/japan-all-stock-financial-results.csv'\
+os.system(
+        'nkf
+         -w
+         --overwrite
+         /Users/shin/Downloads/japan-all-stock-financial-results.csv'
         )
 
 fi_dt = datetime.fromtimestamp(os.stat('/Users/shin/Downloads/japan-all-stock-financial-results.csv').st_mtime)
 fi_get_time = fi_dt.strftime('%Y-%m-%d %H:%M:%S')
 
 columns_fi = [\
-        'st_code','name','kessannki','kessannhappyoubi','uriagedaka','eigyourieki','keijyourieki',\
-        'toukirieki','sousisann','jikosihonn','sihonnkinn','yuurisihusai','jikosihonnhiritu','roe',\
-        'roa','hakkouzumikabusikisuu'\
+        'st_code','name','kessannki','kessannhappyoubi','uriagedaka','eigyourieki','keijyourieki',
+        'toukirieki','sousisann','jikosihonn','sihonnkinn','yuurisihusai','jikosihonnhiritu','roe',
+        'roa','hakkouzumikabusikisuu'
         ]
 
-fi_df = pd.read_csv(\
-        '/Users/shin/Downloads/japan-all-stock-financial-results.csv', \
-        skiprows = 1, \
-        na_values=('-',''), \
-        names = columns_fi \
+fi_df = pd.read_csv(
+        '/Users/shin/Downloads/japan-all-stock-financial-results.csv',
+         skiprows = 1,
+         na_values=('-',''),
+         names = columns_fi
         )
 
 n = len(fi_df)
@@ -47,9 +47,9 @@ fi_df['fi_get_time'] = pd.to_datetime(fi_df['fi_get_time'])
 engine = create_engine('postgresql://shinya@localhost:5432/stock')
 
 fi_df.to_sql(\
-        'jp_al_st_financial', \
-        engine, \
-        if_exists = 'append', \
-        index=False\
+        'jp_al_st_financial',
+        engine,
+        if_exists = 'append',
+        index=False
         )
 
